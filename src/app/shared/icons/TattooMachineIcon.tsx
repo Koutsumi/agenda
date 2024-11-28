3eb3ad82c79d7ecfb226b5b098a738e70bdd6295
Fernanda <febaccarini@gmail.com>
2024-11-27 21:28:43 -0300

    feat icons component

---
+++ src/app/shared/icons/TattooMachineIcon.tsx
@@ -0,0 +1,19 @@
+export default function TattooMachineIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="m7 21-4.3-4.3c-1-1-1-2.5 0-3.4l9.6-9.6c1-1 2.5-1 3.4 0l5.6 5.6c1 1 1 2.5 0 3.4L13 21" />
+        <path d="M22 21 2 1" />
+      </svg>
+    )
+  }
