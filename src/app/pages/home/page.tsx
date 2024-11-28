53e0277789becbe3558b07ecc097411077176607
Fernanda <febaccarini@gmail.com>
2024-11-27 22:02:10 -0300

    feat add header to home page

---
+++ src/app/pages/home/page.tsx
@@ -0,0 +1,10 @@
+import HomeHeader from "@/app/modules/home/components/HomeHeader";
+
+export default function HomePage(){
+    return(
+        <>
+            <HomeHeader/>
+        </>
+    )
+
+}
