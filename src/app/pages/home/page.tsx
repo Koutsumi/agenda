533f4b59b810df4f3c7834163dd82c14b7284839
Fernanda <febaccarini@gmail.com>
2024-12-02 21:17:28 -0300

    add banner to home page

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,9 +1,11 @@
+import Banner from "@/app/modules/home/components/Banner";
 import HomeHeader from "@/app/modules/home/components/HomeHeader";
 
 export default function HomePage(){
     return(
         <>
             <HomeHeader/>
+            <Banner/>
         </>
     )
 
