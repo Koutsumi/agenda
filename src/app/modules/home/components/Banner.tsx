1540528847b9dcb15ab6cd7ba7bf1091d2f7c0ac
Fernanda <febaccarini@gmail.com>
2024-12-04 21:45:16 -0300

    fix bg color

--- src/app/modules/home/components/Banner.tsx
+++ src/app/modules/home/components/Banner.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Banner(){
     return(
-       <section className="w-full flex flex-col items-center justify-center">
+       <section className="w-full flex flex-col items-center justify-center bg-primary">
             <div className="w-[90%] md:w-[35%] flex flex-col items-center justify-center my-[4em]">
                 <h2 className="text-4xl mb-4">Otimize seu estúdio de Tatuagem</h2>
                 <p className="text-center">Gerencie agendamentos, acompanhe dados dos clientes e impulsione seu negócio de tatuagem e piercing com nosso aplicativo especializado de agenda.</p>
