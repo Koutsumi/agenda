45661c2af8f7f1274363333944d8b6a1cc064b16
Fernanda <febaccarini@gmail.com>
2024-12-17 20:46:15 -0300

    reafct add lucide-react icons

--- src/app/modules/home/components/Card.tsx
+++ src/app/modules/home/components/Card.tsx
@@ -1,17 +1,18 @@
+import { LucideIcon } from 'lucide-react';
 import React from 'react';
 
 interface CardProps {
   title: string;
   description: string;
-  icon: JSX.Element;
+  icon: LucideIcon;
   studio?: string
 }
 
-const Card: React.FC<CardProps> = ({ title, description, icon, studio }) => {
+const Card: React.FC<CardProps> = ({ title, description, icon: Icon, studio }) => {
   return (
     <div className="border border-red-500 p-6 rounded-lg shadow-md bg-white">
       <div className="flex items-center justify-center w-12 h-12  text-red-500 rounded-full mb-4">
-        {icon}
+        <Icon/>
       </div>
       <h3 className="text-lg font-semibold text-gray-800 mb-2">{title}</h3>
       <p className="text-sm text-gray-600">{description}</p>
