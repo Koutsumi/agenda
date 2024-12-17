e7276d6dae7b91c4a3672485bf34b5b3f2d37ac4
Fernanda <febaccarini@gmail.com>
2024-12-16 21:32:41 -0300

    feat add studio name

--- src/app/modules/home/components/Card.tsx
+++ src/app/modules/home/components/Card.tsx
@@ -4,16 +4,22 @@ interface CardProps {
   title: string;
   description: string;
   icon: JSX.Element;
+  studio?: string
 }
 
-const Card: React.FC<CardProps> = ({ title, description, icon }) => {
+const Card: React.FC<CardProps> = ({ title, description, icon, studio }) => {
   return (
     <div className="border border-red-500 p-6 rounded-lg shadow-md bg-white">
-      <div className="flex items-center justify-center w-12 h-12 bg-red-100 text-red-500 rounded-full mb-4">
+      <div className="flex items-center justify-center w-12 h-12  text-red-500 rounded-full mb-4">
         {icon}
       </div>
       <h3 className="text-lg font-semibold text-gray-800 mb-2">{title}</h3>
       <p className="text-sm text-gray-600">{description}</p>
+      {
+        studio && (
+          <p className='text-secondary mt-2'>- {studio}</p>
+        )
+      }
     </div>
   );
 };
