4e6541c233d96a9d79c9bffc5fa1ac016fb4333b
Fernanda <febaccarini@gmail.com>
2024-12-16 21:33:36 -0300

    feat home icons

---
+++ src/app/shared/icons/HomeIcons.tsx
@@ -0,0 +1,112 @@
+const icons = {
+    calendar: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M8 7V3m8 4V3m-9 8h10m-9 4h6m7-10h-3M6 7H3m0 0h18a2 2 0 012 2v12a2 2 0 01-2 2H3a2 2 0 01-2-2V9a2 2 0 012-2z"
+        />
+      </svg>
+    ),
+    customize: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.232 5.232l-4.5 4.5m1.5-1.5l3-3m-3 3l-1.5 1.5M21 16.5v2.25a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18.75V16.5M18 9l3 3m-3-3l-1.5 1.5m1.5-1.5V6m-1.5 4.5h3"
+        />
+      </svg>
+    ),
+    clock: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 6v6l4 2m-4-10a9 9 0 100 18 9 9 0 000-18z"
+        />
+      </svg>
+    ),
+    analytics: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M11 6.5V20m4-12v12m-8-7v7"
+        />
+      </svg>
+    ),
+    fastScheduling: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M13.5 4.5H21m-9 6H21m-9 6h7.5m0 0l2.25-2.25M19.5 16.5l2.25 2.25M5.25 18.75L3 16.5m2.25 2.25L5.25 6M3 16.5l7.5-7.5"
+        />
+      </svg>
+    ),
+    profile: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 11a4 4 0 100-8 4 4 0 000 8zm0 2a7 7 0 017 7v2H5v-2a7 7 0 017-7z"
+        />
+      </svg>
+    ),
+
+    star: (
+    <svg width="100" height="100" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <polygon
+        points="12,2 15,9 22,9 17,14 19,21 12,17 5,21 7,14 2,9 9,9"
+        fill="none"
+        stroke="red"
+        strokeWidth="1"
+      />
+    </svg>
+    )
+  };
+  
+  export default icons;
+  
