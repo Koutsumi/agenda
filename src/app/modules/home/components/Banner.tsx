7a6324540d42d72893a80ce98c533a5130e097af
Fernanda <febaccarini@gmail.com>
2024-12-19 21:44:43 -0300

    fix hover button size

--- src/app/modules/home/components/Banner.tsx
+++ src/app/modules/home/components/Banner.tsx
@@ -4,7 +4,7 @@ export default function Banner(){
             <div className="w-[90%] md:w-[35%] flex flex-col items-center justify-center my-[4em]">
                 <h2 className="text-4xl mb-4">Otimize seu estúdio de Tatuagem</h2>
                 <p className="text-center">Gerencie agendamentos, acompanhe dados dos clientes e impulsione seu negócio de tatuagem e piercing com nosso aplicativo especializado de agenda.</p>
-                <a href="" className="bg-secondary py-2 px-4 rounded-lg mt-4 hover:bg-white hover:text-secondary hover:border-2 hover:border-secondary"><button>Agendar demonstração</button></a>
+                <a href="" className="bg-secondary py-2 px-4 rounded-lg mt-4 hover:bg-white hover:text-secondary hover:border-2 border-2 hover:border-secondary border-secondary"><button>Agendar demonstração</button></a>
             </div>
        </section>
     )
